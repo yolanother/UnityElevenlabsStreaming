b731880c846b66ac655b6d0c92471e9d7a918b8c
Yolan <yolanother@gmail.com>
2023-08-14 09:02:49 -0700

    Improve streaming and add character callbacks

--- Scripts/Runtime/Audio/PcmAudioPlayer.cs
+++ Scripts/Runtime/Audio/PcmAudioPlayer.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Concurrent;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Doubtech.ElevenLabs.Streaming
 {
@@ -9,48 +11,180 @@ namespace Doubtech.ElevenLabs.Streaming
     {
         [SerializeField] private AudioSource _audioSource;
 
-        public class PcmAudio
-        {
-            public float[] data;
-            public int channels;
-            public int frequency;
-        }
+        private AudioClip streamingClip;
+        private List<float> audioBuffer = new List<float>();
+        private float elapsedAudioTime = 0;
+
+        private bool clipCompleted = false;
+        private bool starting = false;
+        private bool streaming = false;
+
+        public event Action OnClipStart;
+        public event Action OnClipStop;
+
+        private List<ITimedCallback> timeListeners = new List<ITimedCallback>();
+        private int frequency;
+        private int channels;
+        private float totalDataDuration = 0;
         
-        private ConcurrentQueue<AudioClip> _pcmChunks = new ConcurrentQueue<AudioClip>();
+        private ConcurrentQueue <ITimedCallback> _callbackQueue = new ConcurrentQueue<ITimedCallback>(); 
 
         private void Awake()
         {
-            if(!_audioSource) _audioSource = GetComponent<AudioSource>();
-        }
-        
-        public void PlayPcm(byte[] buffer, int channels, int frequency)
-        {
-            var audioData = new PcmAudio()
-            {
-                data = ConvertByteArrayToFloatArray(buffer),
-                channels = channels,
-                frequency = frequency
-            };
-            AudioClip clip = AudioClip.Create("PCM_Audio", audioData.data.Length, audioData.channels, audioData.frequency, false);
-            clip.SetData(audioData.data, 0);
-            _pcmChunks.Enqueue(clip);
-            
-            StopAllCoroutines();
-            StartCoroutine(FlushQueue());
+            if (!_audioSource) _audioSource = GetComponent<AudioSource>();
         }
 
-        public IEnumerator FlushQueue()
+        public bool IsStreaming => streaming;
+        public float Length => totalDataDuration;
+        public float CurrentTime => elapsedAudioTime;
+
+        public void StartClip(int channels, int frequency)
         {
-            while (_pcmChunks.Count > 0)
+            // Initialize or clear the audio buffer
+            audioBuffer.Clear();
+
+            // Create a streaming AudioClip
+            streamingClip =
+                AudioClip.Create("Streaming_PCM_Audio", int.MaxValue, channels, frequency, true, OnAudioRead);
+            this.frequency = frequency;
+            this.channels = channels;
+            _audioSource.clip = streamingClip;
+            _audioSource.loop = false;
+            _audioSource.Play();
+            elapsedAudioTime = 0;
+            clipCompleted = false;
+            starting = true;
+            streaming = true;
+            totalDataDuration = 0;
+            _callbackQueue.Clear();
+            timeListeners.Clear();
+
+            StartCoroutine(TriggerTiming());
+        }
+
+        public void CompleteClip()
+        {
+            clipCompleted = true;
+            TryClose();
+        }
+
+        public void AddListener(ITimedCallback callback)
+        {
+            timeListeners.Add(callback);
+            // Sort the time listeners by time in reverse order
+            timeListeners.Sort((a, b) => b.Time.CompareTo(a.Time));
+        }
+
+        public void AddPcmData(byte[] buffer, List<ITimedCallback> callbacks = null)
+        {
+            float[] audioData = ConvertByteArrayToFloatArray(buffer);
+            lock (audioBuffer)
             {
-                yield return new WaitUntil(() => !_audioSource.isPlaying);
-                if (_pcmChunks.TryDequeue(out var pcm))
+                audioBuffer.AddRange(audioData);
+            }
+
+            // Calculate the duration of the added data
+            float addedDataDuration = audioData.Length / (float)(frequency * channels);
+            totalDataDuration += addedDataDuration;
+
+            if (callbacks != null)
+            {
+                foreach (var callback in callbacks)
                 {
-                    _audioSource.clip = pcm;
-                    _audioSource.Play();
+                    // Adjust the trigger time relative to the total data duration
+                    float adjustedTriggerTime = totalDataDuration - addedDataDuration + callback.Time;
+                    
+                    AddListener(new AdjustedTimeCallback(adjustedTriggerTime, callback));
                 }
+            }
+        }
+
+        private void OnAudioRead(float[] data)
+        {
+            if (starting)
+            {
+                OnClipStart?.Invoke();
+                starting = false;
+            }
+
+            lock (audioBuffer)
+            {
+                int count = Mathf.Min(data.Length, audioBuffer.Count);
+                audioBuffer.CopyTo(0, data, 0, count);
+                audioBuffer.RemoveRange(0, count);
+                // If count < data.Length, fill the rest of the data with zeroes using an effiecient Array fill
+                Array.Clear(data, count, data.Length - count);
+            }
+
+            // Calculate elapsed time for this chunk
+            float chunkDuration = data.Length / (float)frequency;
+            elapsedAudioTime += chunkDuration;
+            /*for (int i = timeListeners.Count - 1; i >= 0; i--)
+            {
+                if (elapsedAudioTime >= timeListeners[i].Time)
+                {
+                    _callbackQueue.Enqueue(timeListeners[i]);
+                }
+            }*/
+
+            TryClose();
+        }
+
+        private IEnumerator TriggerTiming()
+        {
+            Debug.Log("Tracking playback.");
+            
+            while (streaming)
+            {
+                FlushCallbacks();
+
                 yield return null;
             }
+            
+            FlushCallbacks();
+            Debug.Log("Stopped tracking playback at " + _audioSource.time);
+        }
+
+        private void FlushCallbacks()
+        {
+            if (_callbackQueue.TryDequeue(out var callback))
+            {
+                callback.Invoke();
+                timeListeners.Remove(callback);
+            }
+
+            if (_callbackQueue.Count == 0)
+            {
+                // Check and invoke listeners
+                for (int i = timeListeners.Count - 1; i >= 0; i--)
+                {
+                    if (_audioSource.time >= timeListeners[i].Time)
+                    {
+                        timeListeners[i].Invoke();
+                        timeListeners.RemoveAt(i);
+                    }
+                }
+            }
+        }
+
+        private void TryClose()
+        {
+            // Check for clip completion
+            if (clipCompleted && audioBuffer.Count == 0)
+            {
+                OnClipStop?.Invoke();
+                clipCompleted = false;
+                streaming = false;
+                _audioSource.Stop();
+                Debug.Log("Stopped playback.");
+            }
+        }
+
+        public void PlayPcm(byte[] buffer, int channels, int frequency)
+        {
+            StartClip(channels, frequency);
+            AddPcmData(buffer);
+            CompleteClip();
         }
 
         private float[] ConvertByteArrayToFloatArray(byte[] audioBytes)
@@ -69,8 +203,119 @@ namespace Doubtech.ElevenLabs.Streaming
         }
     }
 
+    public interface ITimedCallback
+    {
+        float Time { get; }
+        void Invoke();
+    }
+
+    public class TimedCallbackUnityEvent : UnityEvent, ITimedCallback
+    {
+        public float Time { get; set; }
+    }
+
+    /// <summary>
+    /// Triggered at a different time than the original callback. Used to handle relative offset time
+    /// </summary>
+    public class AdjustedTimeCallback : ITimedCallback
+    {
+        public float Time { get; set; }
+        public ITimedCallback callback;
+        
+        public AdjustedTimeCallback(float time, ITimedCallback callback)
+        {
+            Time = time;
+            this.callback = callback;
+        }
+        
+        public void Invoke()
+        {
+            callback.Invoke();
+        }
+    }
+
+    public class TimedCallback : ITimedCallback
+    {
+        public float Time { get; set; }
+        public Action OnTimeHit { get; set; }
+        
+        /// <summary>
+        /// Initialize a timed callback
+        /// </summary>
+        /// <param name="time">In seconds</param>
+        /// <param name="onTimeHit"></param>
+        public TimedCallback(float time, Action onTimeHit)
+        {
+            Time = time;
+            OnTimeHit = onTimeHit;
+        }
+        
+        public void Invoke()
+        {
+            OnTimeHit?.Invoke();
+        }
+
+        public static implicit operator TimedCallback((float, Action) tuple)
+        {
+            return new TimedCallback(
+                tuple.Item1,
+                tuple.Item2
+            );
+        }
+    }
+
     public interface IPcmPlayer
     {
-        public void PlayPcm(byte[] pcmData, int channels, int frequency);
+        /// <summary>
+        /// Triggered when a new audio clip begins playing.
+        /// </summary>
+        event Action OnClipStart;
+
+        /// <summary>
+        /// Triggered when the audio clip stops playing.
+        /// </summary>
+        event Action OnClipStop;
+
+        /// <summary>
+        /// Indicates whether the player is currently streaming audio.
+        /// </summary>
+        bool IsStreaming { get; }
+
+        /// <summary>
+        /// Returns the total length of the currently playing or buffered audio clip in seconds.
+        /// </summary>
+        float Length { get; }
+
+        /// <summary>
+        /// Returns the current playback time of the audio clip in seconds.
+        /// </summary>
+        float CurrentTime { get; }
+
+        /// <summary>
+        /// Initializes a new audio clip with the specified channels and frequency.
+        /// </summary>
+        /// <param name="channels">Number of audio channels.</param>
+        /// <param name="frequency">Sample frequency of the audio.</param>
+        void StartClip(int channels, int frequency);
+
+        /// <summary>
+        /// Indicates that no more data will be added to the audio clip, marking it as complete.
+        /// </summary>
+        void CompleteClip();
+
+        /// <summary>
+        /// Adds PCM data to the audio buffer. Optional callbacks can be provided to be triggered at specific times relative to the added data.
+        /// </summary>
+        /// <param name="buffer">The PCM data buffer to add.</param>
+        /// <param name="callbacks">List of callbacks with their associated trigger times relative to the start of the added data.</param>
+        void AddPcmData(byte[] buffer, List<ITimedCallback> callbacks = null);
+
+        /// <summary>
+        /// Starts playback of a PCM audio buffer with the specified channels and frequency.
+        /// </summary>
+        /// <param name="pcmData">PCM audio data to play.</param>
+        /// <param name="channels">Number of audio channels.</param>
+        /// <param name="frequency">Sample frequency of the audio.</param>
+        void PlayPcm(byte[] pcmData, int channels, int frequency);
     }
 }
--- Scripts/Runtime/ElevenLabsPCMWrapperStreamer.cs
+++ Scripts/Runtime/ElevenLabsPCMWrapperStreamer.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using DoubTech.Elevenlabs.Streaming;
 using DoubTech.Elevenlabs.Streaming.NativeWebSocket;
@@ -17,14 +20,22 @@ namespace Doubtech.ElevenLabs.Streaming
 
         private string _url = "ws://{0}:{1}/ws/synthesize?apikey={2}&voice={3}";
         private WebSocket _webSocket;
-        private IPcmPlayer[] _players;
+        private IPcmPlayer _player;
         private bool _open;
         private bool _ready;
         public string Url => string.Format(_url, _host, _port, _apiKey, _voiceId);
+        
+        private class WordTrigger {
+            public string word;
+            public Action<string> onStartChunk;
+            public Action<string> onFinishChunk;
+        }
+        
+        private List<WordTrigger> wordStack = new List<WordTrigger>();
 
         private void Start()
         {
-            _players = GetComponents<IPcmPlayer>();
+            _player = GetComponent<IPcmPlayer>();
         }
 
         [Button]
@@ -49,7 +60,7 @@ namespace Doubtech.ElevenLabs.Streaming
             _webSocket.OnClose += OnClose;
             await _webSocket.Connect();
             
-            while (!_ready)
+            while (!_ready && enabled)
             {
                 await Task.Yield();
             }
@@ -65,7 +76,6 @@ namespace Doubtech.ElevenLabs.Streaming
         private void OnMessage(byte[] msg)
         {
             var text = Encoding.UTF8.GetString(msg);
-            Debug.Log("OnMessage: " + text);
             var json = JSON.Parse(text);
             if (!_ready)
             {
@@ -77,11 +87,92 @@ namespace Doubtech.ElevenLabs.Streaming
                 var channels = json["audio"]["channels"];
                 var frequency = json["audio"]["frequency"];
                 var audioBytes = System.Convert.FromBase64String(audio);
-                foreach(var player in _players)
+                if(!_player.IsStreaming) _player.StartClip(channels, frequency);
+                
+                // Create callbacks for each played letter
+                List<ITimedCallback> callbacks = new List<ITimedCallback>();
+                if (json.HasKey("normalizedAlignment")
+                    && json["normalizedAlignment"].HasKey("chars")
+                    && json["normalizedAlignment"].HasKey("charStartTimesMs")
+                    && json["normalizedAlignment"].HasKey("charDurationsMs"))
                 {
-                    player.PlayPcm(audioBytes, channels, frequency);
+                    var chars = json["normalizedAlignment"]["chars"].AsArray;
+                    var times = json["normalizedAlignment"]["charStartTimesMs"].AsArray;
+                    var durations = json["normalizedAlignment"]["charDurationsMs"].AsArray;
+                    
+                    // Combine characters into string
+                    var sb = new StringBuilder();
+                    for(int i = 0; i < chars.Count; i++)
+                    {
+                        sb.Append(chars[i].Value);
+                    }
+                    // Add a callback for the first letter
+                    float first = times[0].AsInt / 1000f;
+                    callbacks.Add(new TimedCallback(first, () =>
+                    {
+                        Debug.Log("Started playing " + sb.ToString());
+                    }));
+
+                    StringBuilder word = new StringBuilder();
+                    for (int i = 0; i < chars.Count; i++)
+                    {
+                        string c = chars[i].Value;
+                        float t = times[i].AsInt / 1000f;
+                        float d = durations[i].AsInt / 1000f;
+                        callbacks.Add(new TimedCallback(t, () =>
+                        {
+                            Debug.Log($"Played {c} at {t} for {d}");
+                            Debug.Log("Wordstack: " + string.Join(", ", wordStack.Select(w => w.word)));
+
+                        }));
+                        
+                        // If c is Regex word character
+                        if (Regex.IsMatch(c, @"\w"))
+                        {
+                            word.Append(c);
+                        }
+                        else
+                        {
+                            Debug.Log("Word: " + word.ToString());
+                            if (wordStack.Count > 0 && wordStack[0].word == word.ToString())
+                            {
+                                var wordTrigger = wordStack[0];
+                                wordStack.RemoveAt(0);
+                                if(null != wordTrigger.onStartChunk) callbacks.Add(new TimedCallback(0, () => wordTrigger.onStartChunk(word.ToString())));
+                                if(null != wordTrigger.onFinishChunk) callbacks.Add(new TimedCallback(0, () => wordTrigger.onFinishChunk(word.ToString())));
+                            }
+                            word.Clear();
+                        }
+                    }
+                    if (wordStack.Count > 0 && wordStack[0].word == word.ToString())
+                    {
+                        var wordTrigger = wordStack[0];
+                        wordStack.RemoveAt(0);
+                        if(null != wordTrigger.onStartChunk) callbacks.Add(new TimedCallback(0, () => wordTrigger.onStartChunk(word.ToString())));
+                        if(null != wordTrigger.onFinishChunk) callbacks.Add(new TimedCallback(0, () => wordTrigger.onFinishChunk(word.ToString())));
+                    }
+                    
+                    // Add a callback for after the last letter
+                    float last = (times[times.Count - 1].AsInt + durations[durations.Count - 1].AsInt) / 1000f;
+                    callbacks.Add(new TimedCallback(last, () =>
+                    {
+                        Debug.Log("Finished playing " + sb.ToString());
+                        // Print the wordstack use linq to join the words
+                        Debug.Log("Wordstack: " + string.Join(", ", wordStack.Select(w => w.word)));
+                        
+                    }));
                 }
+                
+                _player.AddPcmData(audioBytes, callbacks);
+                json["audio"]["data"] = "Read and played.";
             }
+
+            Debug.Log("OnMessage: \n" + json.ToString());
+        }
+
+        private void OnTimeHit()
+        {
+            throw new NotImplementedException();
         }
 
         private void OnError(string errorMsg)
@@ -103,20 +194,9 @@ namespace Doubtech.ElevenLabs.Streaming
         [Button]
         public async void Send(string text)
         {
-            if (null == _webSocket) await Connect();
-            else if (_webSocket.State != WebSocketState.Open) await Connect();
-            if (null == _webSocket)
-            {
-                Debug.LogError("Not connected.");
-                return;
-            }
-            Debug.Log("Sending BOM");
-            await _webSocket.SendText("{}");
-            var json = new JSONObject();
-            json["text"] = text;
-            json["final"] = true;
-            Debug.Log("Sending text: " + text);
-            await _webSocket.SendText(json.ToString());
+            await StartStreamAsync();
+            await SendChunkAsync(text);
+            await EndStreamAsync();
         }
 
         [Button]
@@ -132,7 +212,7 @@ namespace Doubtech.ElevenLabs.Streaming
         
         public async Task SendFinalAsync(string text)
         {
-            await SendChunkAsync(text);
+            if(!string.IsNullOrEmpty(text)) await SendChunkAsync(text);
             await EndStreamAsync();
         }
 
@@ -140,18 +220,54 @@ namespace Doubtech.ElevenLabs.Streaming
         {
             if (null == _webSocket) await Connect();
             else if (_webSocket.State != WebSocketState.Open) await Connect();
+            if (!enabled) return;
             var json = new JSONObject();
+            wordStack.Clear();
             await _webSocket.SendText(json.ToString());
         }
 
         public async Task SendChunkAsync(string text)
         {
             if (null == _webSocket) await StartStreamAsync();
+            if (!enabled) return;
             var json = new JSONObject();
             json["text"] = text;
+            
+            // Get all of the words in the text and ignore empty strings and trim any whitespace
+            var regex = new Regex(@"(\w+)", RegexOptions.Compiled);
+            var matches = regex.Matches(text);
+            var words = matches.Select(m => m.Value.Trim()).Where(s => !string.IsNullOrEmpty(s)).ToArray();
+            for (int i = 0; i < words.Length; i++)
+            {
+                var word = new WordTrigger
+                {
+                    word = words[i]
+                };
+                if (i == 0)
+                {
+                    word.onStartChunk = (s) => OnStartChunk(text);
+                }
+
+                if (i == words.Length - 1)
+                {
+                    word.onFinishChunk = (s) => OnFinishChunk(text);
+                }
+                wordStack.Add(word);
+            }
+            
             await _webSocket.SendText(json.ToString());
         }
-        
+
+        private void OnFinishChunk(string text)
+        {
+            Debug.Log("OnFinishChunk: " + text);
+        }
+
+        private void OnStartChunk(string text)
+        {
+            Debug.Log("OnStartChunk: " + text);
+        }
+
         public async Task EndStreamAsync()
         {
             if (null == _webSocket) return;
@@ -159,6 +275,7 @@ namespace Doubtech.ElevenLabs.Streaming
             var json = new JSONObject();
             json["final"] = true;
             await _webSocket.SendText(json.ToString());
+            _player.CompleteClip();
         }
 
         private void Update()
