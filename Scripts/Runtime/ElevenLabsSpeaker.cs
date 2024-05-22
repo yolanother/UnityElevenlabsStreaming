94a0a2efbbb5c9c9f18dcf86fc830c8dd246c4b7
Aaron Jackson <akjackson@meta.com>
2024-05-22 12:01:08 -0700

    Add missing using

--- Scripts/Runtime/ElevenLabsSpeaker.cs
+++ Scripts/Runtime/ElevenLabsSpeaker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Doubtech.ElevenLabs.Streaming.Interfaces;
